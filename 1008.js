5cfde852d2938b1e7fa9b72d34cc80fcf0c4124c
YoungCheol Lee <ofetwo@gmail.com>
2023-09-09 21:06:32 +0900

    prob 1008

---
+++ 1008.js
@@ -0,0 +1,14 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('', (str) => {
+    let splitted = str.trim().split(' ')
+    splitted = splitted.filter((ele) => ele !== '')
+    const [A, B] = splitted.map((ele) => parseInt(ele))
+    console.log(A / B)
+    rl.close()
+})
