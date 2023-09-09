882c1c124e282b93405af0bbf3ea34a52cadb86a
YoungCheol Lee <ofetwo@gmail.com>
2023-09-09 21:24:59 +0900

    prob 10869

---
+++ 10869.js
@@ -0,0 +1,18 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('', (str) => {
+    let splitted = str.trim().split(' ')
+    splitted = splitted.filter((ele) => ele !== '')
+    const [A, B] = splitted.map((ele) => parseInt(ele))
+    console.log(A + B)
+    console.log(A - B)
+    console.log(A * B)
+    console.log(Math.floor(A / B))
+    console.log(A % B)
+    rl.close()
+})
