d0be27469d1ee38ade16ea2e44fff3228ac50915
YoungCheol Lee <ofetwo@gmail.com>
2023-09-10 00:05:04 +0900

    prob 2588

---
+++ 2588.js
@@ -0,0 +1,20 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('', (str1) => {
+    // let trimmed = str.trim()
+    let num1 = parseInt(str1.trim())
+    rl.question('', (str2) => {
+        let trimmed = str2.trim()
+        let num2 = parseInt(trimmed)
+        let num2DigitsArr = trimmed.split('').map((ele) => parseInt(ele))
+        num2DigitsArr.reverse().forEach((digit) => console.log(num1 * digit))
+        console.log(num1 * num2)
+        rl.close()
+    })
+
+})
