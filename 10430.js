0e69ebe96fbe70917bbc9ba8187d05ba1cb55503
YoungCheol Lee <ofetwo@gmail.com>
2023-09-09 23:22:34 +0900

    prob 10430

---
+++ 10430.js
@@ -0,0 +1,17 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('', (str) => {
+    let splitted = str.trim().split(' ')
+    splitted = splitted.filter((ele) => ele !== '')
+    const [A, B, C] = splitted.map((ele) => parseInt(ele))
+    console.log((A + B) % C)
+    console.log(((A % C) + (B % C)) % C)
+    console.log((A * B) % C)
+    console.log(((A % C) * (B % C)) % C)
+    rl.close()
+})
