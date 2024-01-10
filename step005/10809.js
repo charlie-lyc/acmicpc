9c07a4ea577f542cb5c0fb1379a028f4003dc494
YoungCheol Lee <53360738+charlie-lyc@users.noreply.github.com>
2024-01-10 13:46:48 +0000

    step005

--- step005/10809.js
+++ step005/10809.js
@@ -3,19 +3,31 @@ const rl = require('readline').createInterface({
     output: process.stdout
 })
 
+/* One Solution */
 let S = ''
 let result = []
-for (let i = 0; i < 26; i ++) {
-    result.push(-1)
-}
-
 rl.on('line', (str) => {
     S = str.trim()
-    for (let j = 0; j < S.length; j ++) {
-        const idx = S[j].charCodeAt(0) % 97
-        if (result[idx] === -1) {
-            result[idx] = j
-        }
+    for (let i = 97; i < 123; i ++) {
+        const idx = S.indexOf(String.fromCharCode(i))
+        result.push(idx)
     }
     rl.close()
 }).on('close', () => console.log(...result))
+
+/* Another Solution */
+// let S = ''
+// let result = []
+// for (let i = 0; i < 26; i ++) {
+//     result.push(-1)
+// }
+// rl.on('line', (str) => {
+//     S = str.trim()
+//     for (let j = 0; j < S.length; j ++) {
+//         const idx = S[j].charCodeAt(0) % 97
+//         if (result[idx] === -1) {
+//             result[idx] = j
+//         }
+//     }
+//     rl.close()
+// }).on('close', () => console.log(...result))
